b1ea15758309da35badc977e2780c3abe73db768
Tommote <1427553459@qq.com>
2017-12-10 05:24:13 +0800

    0.100

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,33 +1,40 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+
+
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    dengluhou:true,
+    bigpicsrc : '/img/bingmayong.jpg',
+    modepic: "scaleToFill",
+    smallpic:'/img/dayanta.jpg'
   },
   //事件处理函数
   bindViewTap: function() {
     this.setData({
       motto :"this is a page"
     })
-    console.log("sdsd")  
+   
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
-      wx.navigateTo({
-        url: '../login/login',
-      })
+     // wx.navigateTo({
+      //  url: '../login/login',
+      //})
 
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
+        dengluhou : false
       })
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -35,7 +42,8 @@ Page({
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
+          dengluhou: false
         })
       }
     } else {
@@ -45,7 +53,8 @@ Page({
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: true,
+            dengluhou: false
           })
         }
       })
@@ -57,7 +66,8 @@ Page({
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
+      dengluhou: false
     })
   }
 })
